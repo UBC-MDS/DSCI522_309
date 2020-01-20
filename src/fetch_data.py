3e551ed06e256e81a0074e0b5059ef1dac0e3d1a
Marvin-chengmin <marvin.cmin@gmail.com>
2020-01-19 16:58:06 -0800

    modify the script to take localpath as input

--- src/fetch_data.py
+++ src/fetch_data.py
@@ -7,35 +7,44 @@
    After fetching the data, the data is splitted into training and testing set and 
    stored in the data folder
 
-   Usage: fetch_data.py [--url=<url>] [--test_size=<test_size>]
+   Usage: src/fetch_data.py --localpath=<localpath> [--url=<url>]
 
    Options:
+   --localpath=<localpath> Local file path to write the data
    --url=<url> The URL path to the csv file [default: "https://archive.ics.uci.edu/ml/machine-learning-databases/00468/online_shoppers_intention.csv"]
-   --test_size=<test_size> The size of the test size [default: 0.2]
 
 """
 import numpy as np
 import pandas as pd
+import os
 from sklearn.model_selection import train_test_split
 from docopt import docopt
 
 opt = docopt(__doc__)
 
-def main(url, test_size):
-    # Read the data
-    raw_data = pd.read_csv(url)
+def main(localpath, url):
+
+   # Check the local path
+   if not os.path.exists(localpath):
+        os.makedirs(localpath)
+   
+   # Read the data
+   raw_data = pd.read_csv(url)
     
-    # Split the data
-    X_train, X_test, y_train, y_test = train_test_split(raw_data.drop(columns='Revenue'), 
+   # Split the data
+   X_train, X_test, y_train, y_test = train_test_split(raw_data.drop(columns='Revenue'), 
                                                     raw_data['Revenue'], 
-                                                    test_size=test_size, 
+                                                    test_size=0.2, 
                                                     random_state=522)
-    
-    # Save the data
-    X_train.to_csv('data/X_train.csv', index=False)
-    pd.DataFrame(y_train).to_csv('data/y_train.csv', index=False)
-    X_test.to_csv('data/X_test.csv', index=False)
-    pd.DataFrame(y_test).to_csv('data/y_test.csv', index=False)
+   localpath_x_train = localpath + "/X_train.csv"
+   localpath_y_train = localpath + "/y_train.csv"
+   localpath_x_test = localpath + "/X_test.csv"
+   localpath_y_test = localpath + "/y_test.csv"
+   # Save the data
+   X_train.to_csv(localpath_x_train, index=False)
+   pd.DataFrame(y_train).to_csv(localpath_y_train, index=False)
+   X_test.to_csv(localpath_x_test, index=False)
+   pd.DataFrame(y_test).to_csv(localpath_y_test, index=False)
 
 def test_url(url):
    test = np.DataSource()
@@ -44,11 +53,10 @@ def test_url(url):
 # Set the default values for inputs
 if opt["--url"] is None:
    opt["--url"] = "https://archive.ics.uci.edu/ml/machine-learning-databases/00468/online_shoppers_intention.csv"
-if opt["--test_size"] is None:
-   opt["--test_size"] = 0.2
    
 test_url(opt["--url"])
 
 if __name__ == "__main__":
-   main(opt["--url"], float(opt["--test_size"]))
+   print(opt)
+   main(opt["--localpath"], opt["--url"])
  
